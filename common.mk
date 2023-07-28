1396d17e86dfcfaccc02050d5b8443467ef440f9
Sugakesshaa <sugakesshaa@gmail.com>
2023-07-29 04:34:00 +0800

    sm8350-common: Separate powerhint for lahaina and yupik
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- common.mk
+++ common.mk
@@ -245,7 +245,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/perf/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Power
